1fa220de09e5d2ae28277d75a82a0a73995c2e2e
Zhigang Wang <zhigangw@hotmail.com>
2020-08-23 22:36:11 -0700

    add dmatrics.py

---
+++ find_SCC/dmatrix.py
@@ -0,0 +1,23 @@
+import numpy as np
+from scipy.sparse import csr_matrix, coo_matrix, csc_matrix
+
+class DMatrix():
+    def __init__(self):
+        data = {}
+        lenght = (int)(863705  / 8)
+        tn = np.ndarray(shape=(lenght), dtype=np.uint8)
+        hn = np.ndarray(shape=(lenght), dtype=np.uint8)
+        dn = np.ndarray(shape=(lenght), dtype=np.uint8)
+        count = 0
+        handle = open('Find_SCC_data.txt')
+        for line in handle:
+            t , h = [int(float(v)) for v in line.split()]    
+            tn[count] = t
+            hn[count] = h
+            dn[count] = 1
+        coo = coo_matrix((dn, (tn, hn)), shape=(lenght, lenght))
+        self.graph = csr_matrix(coo)
+        # self.graph = csc_matrix(coo)
+
+    def transpose():
+        self.graph.transpose()
