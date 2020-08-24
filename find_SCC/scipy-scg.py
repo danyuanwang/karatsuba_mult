7baa17defddc2aead12192cf06dd58ffb213a499
Zhigang Wang <zhigangw@hotmail.com>
2020-08-23 19:29:20 -0700

    update scipy-scg.py

--- find_SCC/scipy-scg.py
+++ find_SCC/scipy-scg.py
@@ -1,25 +1,22 @@
-from scipy.sparse import csr_matrix
+import numpy as np
+from scipy.sparse import csr_matrix, coo_matrix
 from scipy.sparse.csgraph import connected_components
+def read_file():
+    lenght = (int)(863705  / 8)
+    tn = np.ndarray(shape=(lenght), dtype=np.uint8)
+    hn = np.ndarray(shape=(lenght), dtype=np.uint8)
+    dn = np.ndarray(shape=(lenght), dtype=np.uint8)
+    count = 0
+    handle = open('Find_SCC_data.txt')
+    for line in handle:
+        t , h = [int(float(v)) for v in line.split()]    
+        tn[count] = t
+        hn[count] = h
+        dn[count] = 1
+    return coo_matrix((dn, (tn, hn)), shape=(lenght, lenght))
 
-graph = []
-
-handle = open('Find_SCC_data.txt')
-for line in handle:
-    t , h = [int(float(v)) for v in line.split()]
-    while(h >= len(graph) or graph[h] == None):
-        graph.insert(h,[])
-
-    while(t >= len(graph[h])):
-        graph[h].insert(t , 0)
-    
-    graph[h][t] = 1
-
-print(len(graph), len(graph[0]))
-
+graph = read_file()
 graph = csr_matrix(graph)
-
-print(graph)
-
 n_components, labels = connected_components(csgraph=graph, directed=False, return_labels=True)
 
 print(n_components)
