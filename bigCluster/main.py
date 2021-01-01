0fe009b3e9e5414a63d72c7823381e5101bd0b37
Zhigang Wang <zhigangw@hotmail.com>
2021-01-01 09:37:13 -0800

    worked!

--- bigCluster/main.py
+++ bigCluster/main.py
@@ -5,6 +5,14 @@ import sys
 import time
 empty = (2<< 31) - 1
 noparent = (2<< 31) - 2
+
+def countCluster(cluster):
+    i=0
+    for c in cluster:
+        if(c==noparent):
+            i+=1
+    return i
+
 def calculateBigClusters(dataFile):
 
     cluster = array.array('I')
@@ -21,17 +29,18 @@ def calculateBigClusters(dataFile):
             continue
         number = int(line.strip().replace(' ', ''), 2)
         #list = [float(v) for v in line.split()]
-        cluster[number] = noparent
-
+        if cluster[number] == empty:
+            cluster[number] = noparent
+            
         data[count]=number
         count+=1
 
-    clusters = Clusters(cluster)
+    clusters = Clusters(cluster,countCluster(cluster))
     edgeList = Edges(data, cluster)
     #print("len(edgeList.edges[0])",len(edgeList.edges[0]), edgeList.edges[0])
     #print("len(edgeList.edges[1])",len(edgeList.edges[1]), edgeList.edges[1])
     #print("len(edgeList.edges[2])",len(edgeList.edges[2]), edgeList.edges[2])
-    for i in range(3):
+    for i in range(2):
         while len(edgeList.edges[i]) > 0:
             startTime = time.time()
             shortestEdge = edgeList.shortestEdge()
@@ -46,4 +55,5 @@ def calculateBigClusters(dataFile):
 
 startTime = time.time()
 cluster = calculateBigClusters("dataBigCluster.txt")
+print()
 print(cluster, time.strftime("%H:%M:%S", time.gmtime(time.time()-startTime)))
