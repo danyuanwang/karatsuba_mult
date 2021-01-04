a6583dd9e40f564cc0006b2b8eb7506ff0f9a779
danyuanwang <danyuanwang@hotmail.com>
2021-01-03 17:20:06 -0800

    WIS Second commit

--- WISAlgo/WIS.py
+++ WISAlgo/WIS.py
@@ -21,23 +21,27 @@ class WIS():
                 return [self.data[0], n-1]
             self.records[n] = [self.data[1], n]
             return [self.data[1], n]
-        ExcludeW = 0
-        IncludeW =0
+        ExcludeW = None
+        IncludeW = None
         print("weights:", n, self.data[n])
         if n-1 in self.records:
             ExcludeW = self.records[n-1]
         else:
             ExcludeW = self.findWIS(n-1)
+            self.records[n-1] = ExcludeW
 
         if n-2 in self.records:
             IncludeW = self.records[n-2]
         else:
             IncludeW = self.findWIS(n-2)
+            self.records[n-2] = IncludeW
         IncludeW[0] += self.data[n]
         IncludeW.append(n)
 
         if ExcludeW[0] > IncludeW[0]:
             self.records[n] = ExcludeW
+            print("returned excluded", n, ExcludeW, self.records)
             return ExcludeW
         self.records[n] = IncludeW
+        print("returned included", n, IncludeW, self.records)
         return IncludeW
