93d68ff298407b390d1d0b3caa3809982cdf6475
danyuanwang <danyuanwang@hotmail.com>
2021-01-09 22:52:36 -0800

    big KNapsack finished

--- bigKnapsack/knapsack.py
+++ bigKnapsack/knapsack.py
@@ -6,40 +6,43 @@ def decombineNodes(n):
     return [n>>32,n&0xffffffff]
 class Knapsack():
     def __init__(self):
-        self.knapCapacity = 5
-        self.itemCount = 5
+        handle = open('data.txt')
+
         self.data = [empty]
         self.instanceStorage = {}
-        #self.instanceStorage.extend((empty,) * (2000000 * 2000))
-        handle = open('test.txt')
-        flag = True
+        firstline = True
         for line in handle:
-            if flag:
-                flag = False
-                continue
             list = [int(v) for v in line.split()]
-            self.data.append(list)
+            if firstline:
+                [self.knapCapacity, self.itemCount] = list
+                firstline = False
+            else:
+                self.data.append(list)
 
             
     def findOptimum(self, itemCount, knapCapacity):
+        key = combineNote(itemCount,knapCapacity)
+        if key in self.instanceStorage:
+            return self.instanceStorage[key]
+        
+        r = 0
         if itemCount == 0:
-            return 0
-        option1 = 0
-        option2 = 0
+            r = 0
+        else:
+            option1 = 0
+            option2 = 0
 
-        if(combineNote(itemCount - 1, knapCapacity) not in self.instanceStorage):
             option1 = self.findOptimum(itemCount - 1, knapCapacity)
-            self.instanceStorage[combineNote(itemCount - 1, knapCapacity)] = option1
-        else:
-            option1 = self.instanceStorage[combineNote(itemCount - 1, knapCapacity)]
 
-        if self.data[itemCount][1] > knapCapacity:
-            return option1
+            if self.data[itemCount][1] > knapCapacity:
+                r = option1
 
-        if(combineNote(itemCount - 1, knapCapacity - self.data[itemCount][1]) not in self.instanceStorage):
-            option2 = self.findOptimum(itemCount - 1, knapCapacity - self.data[itemCount][1]) + self.data[itemCount][0]
-            self.instanceStorage[combineNote(itemCount - 1, knapCapacity - self.data[itemCount][1])] = option2
-        else:
-            option2 = self.instanceStorage[combineNote(itemCount - 1, knapCapacity - self.data[itemCount][1])]
-        return max(option1, option2)
+            else:
+                option2 = self.findOptimum(itemCount - 1, knapCapacity - self.data[itemCount][1]) + self.data[itemCount][0]
+                r = max(option1, option2)
+
+        print(itemCount, knapCapacity)
+        self.instanceStorage[key] = r
+
+        return r
         
